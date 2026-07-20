409a195b8fdbaf01ad7a73813b1b93aee857ebde
Tanjiro <tushar.goel.ml@gmail.com>
2026-07-20 12:06:30 +0530

    Update model name and adjust load test parameters

--- llm_bench/run_load_test.py
+++ llm_bench/run_load_test.py
@@ -1,20 +1,20 @@
 import os
 
-model_name = "meta-llama/Llama-3.2-1B-Instruct"
+model_name = "Qwen/Qwen3-0.6B"
 users = [1,2,4,8,16,32,64]
 input_toks = [1000]
-output_toks = [1,3,5,7]
-pcmls = [0, 0.5, 0.8, 0.9]
+output_toks = [500]
+pcmls = [0.0]
 for user in users:
     for input_tok in input_toks:
         for output_tok in output_toks:
             for pcml in pcmls:
 
                 pcml = int(pcml*input_tok)
-                url = "http://localhost:8000/v1"
-                token = "sk-xxxxxxxxxxxxxx"
+                url = "https://http.******.clusters.simplismart.tech"
+                token = "<jwt-token>"
 
                 qps = 10
                 max_requests = 5
                 os.system(
-                    f"locust -t 30sec -pcml {pcml} --users {user} -r {user} -o {output_tok} -H {url} -p {input_tok} --api-key {token} --model={model_name} --prompt-randomize --chat --provider openai --stream --temperature 0.0  --header id:f49b2e20-fef3-4441-9358-897f946b8ae2 --summary test-vllm-v5.csv")
+                    f"locust -t 30s -pcml {pcml} --users {user} -r {user} -o {output_tok} -H {url} -p {input_tok} --api-key {token} --model={model_name} --prompt-randomize --chat --provider openai --stream --temperature 0.0  --header id:f49b2e20-fef3-4441-9358-897f946b8ae2 --summary test-vllm-v6-proxy.csv")
